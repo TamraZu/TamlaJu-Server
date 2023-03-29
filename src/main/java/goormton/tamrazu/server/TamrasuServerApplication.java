07132ce9c3c3685517f1b3cd4ce75fe93dfb830b
thguss <thgus345@naver.com>
2023-03-29 15:59:07 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/goormton/tamrazu/server/TamrasuServerApplication.java
@@ -0,0 +1,13 @@
+package goormton.tamrazu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamrasuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamrasuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goormton/tamrazu/server/TamrasuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package goormton.tamrazu.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamrasuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
