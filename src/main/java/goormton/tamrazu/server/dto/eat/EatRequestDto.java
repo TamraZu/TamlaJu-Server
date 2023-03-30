764d844b153c6dd711a266af208d9d73ba53951d
thguss <thgus345@naver.com>
2023-03-30 17:55:44 +0900

    [DEL] 미사용 임포터 삭제

--- src/main/java/goormton/tamrazu/server/dto/eat/EatRequestDto.java
+++ src/main/java/goormton/tamrazu/server/dto/eat/EatRequestDto.java
@@ -1,10 +1,4 @@
 package goormton.tamrazu.server.dto.eat;
 
-import javax.swing.text.html.parser.Entity;
-
-import goormton.tamrazu.server.domain.Alcohol;
-import goormton.tamrazu.server.domain.Eat;
-import goormton.tamrazu.server.domain.Member;
-
 public record EatRequestDto(Long memberId, Long alcoholId) {
 }
--- src/main/java/goormton/tamrazu/server/repository/EatRepository.java
+++ src/main/java/goormton/tamrazu/server/repository/EatRepository.java
@@ -1,12 +1,10 @@
 package goormton.tamrazu.server.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import goormton.tamrazu.server.domain.Alcohol;
-import goormton.tamrazu.server.domain.Category;
 import goormton.tamrazu.server.domain.Eat;
 import goormton.tamrazu.server.domain.Member;
 
