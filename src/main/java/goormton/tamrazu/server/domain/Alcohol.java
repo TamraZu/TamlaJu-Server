27f0bcab6b64ee300aec6a958843d143bc4101b1
thguss <thgus345@naver.com>
2023-03-30 16:51:42 +0900

    [ADD] Enumuated 어노테이션 추가

--- src/main/java/goormton/tamrazu/server/domain/Alcohol.java
+++ src/main/java/goormton/tamrazu/server/domain/Alcohol.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -31,6 +33,8 @@ public class Alcohol {
 	private Long volume;
 	private String description;
 	private String region;
+
+	@Enumerated(EnumType.STRING)
 	private Category category;
 	private int ateCount;
 
