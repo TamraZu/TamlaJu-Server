d4f792f59e3f78b818330edd9047ce1dc5fefb53
thguss <thgus345@naver.com>
2023-04-09 02:01:57 +0900

    [FIX] 앱 이름 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,14 @@
 REPOSITORY=/home/ubuntu/build
+# shellcheck disable=SC2164
 cd $REPOSITORY
 
-APP_NAME=tamrazu
+APP_NAME=server-0.0.1-SNAPSHOT.jar
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -15,8 +16,6 @@ else
   sudo kill -15 $CURRENT_PID
   sleep 5
 fi
-
-sleep 100
 
 echo "> $JAR_PATH 배포"
 nohup java -jar -Dspring.profiles.active=dev /home/ubuntu/build/build/libs/server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
